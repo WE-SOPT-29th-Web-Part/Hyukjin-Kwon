5ccb25b82066a1334ed0ea6666dae06e6c802196
KimKwon <khj9709@hanyang.ac.kr>
2021-10-09 23:10:48 +0900

    feat: :sparkles: 드랍다운 디테일 추가 구현
    1. 드랍다운 외부 클릭 시 toggle
    2. 드랍다운 아이템 클릭 시 선택된 아이템 텍스트 표시 및 컬러

--- velog-clone/main.js
+++ velog-clone/main.js
@@ -1,16 +1,45 @@
 "use strict";
 
+let isAnyDropdownOpen = false;
+
 const dropdowns = document.querySelectorAll('.filters__dd-wrapper');
 const optionsDropdown = document.querySelector('.options > .filters__dd-wrapper .filters__dropdown');
 const menuDropdown = document.querySelector('.menu > .filters__dd-wrapper .filters__dropdown')
 
+const dropdownTextGroup = document.querySelectorAll('ul.filters__dropdown');
+const currentDropDownText = document.querySelector('.filters__dd-current > span');
+
+document.addEventListener('click', (e) => {
+  const target = e.target;
+  const parent = target.closest('li');
+  const isDropdownClicked = parent && (parent.classList.contains('options') || parent.classList.contains('menu'));
+  if (isDropdownClicked || !isAnyDropdownOpen) return false;
+  menuDropdown.classList.remove('open');
+  optionsDropdown.classList.remove('open');
+});
+
 dropdowns.forEach((dropdown) => {
   dropdown.addEventListener('click', (e) => {
-    const target = dropdown.closest('li');
+    const target = dropdown.closest('li.filters__list');
     if (target.classList.contains('options')) {
       optionsDropdown.classList.toggle('open');
     } else {
       menuDropdown.classList.toggle('open');
     }
+    isAnyDropdownOpen = optionsDropdown.classList.contains('open') || menuDropdown.classList.contains('open');
   });
+})
+
+dropdownTextGroup.forEach((dropdownText, ddIndex) => {
+  dropdownText.addEventListener('click', (e) => {
+    const target = e.target;
+    const parent = target.closest('ul');
+    if (parent && parent.classList.contains('filters__dropdown')) {
+      [...parent.children].forEach((child) => {
+        child.classList.remove('selected');
+      })
+      target.classList.add('selected');
+      if (!ddIndex) currentDropDownText.innerText = target.innerText;
+    }
+  })
 })

a4160db06c08221b623b8524409a61ba24542067
KimKwon <khj9709@hanyang.ac.kr>
2021-10-17 03:16:25 +0900

    feat: :sparkles: 슬라이더 페이지네이션 구현

--- velog-clone/src/slider.js
+++ velog-clone/src/slider.js
@@ -3,6 +3,7 @@ const gridOption = document.querySelector('.filters__grid');
 const sliderOption = document.querySelector('.filters__slider');
 const postContainer = document.querySelector('.velog');
 
+let slidePagination = null;
 let leftSlideBtn = null;
 let rightSlideBtn = null;
 let slidePostList = null;
@@ -18,14 +19,18 @@ const movePost = () => {
 
 const moveRight = () => {
   if (slidePostList.length - maxPostCount <= currentIndex) return false;
+  slidePagination.childNodes[currentIndex].classList.remove('current');
   currentIndex += 1;
   movePost();
+  slidePagination.childNodes[currentIndex].classList.add('current');
 }
 
 const moveLeft = () => {
   if (!currentIndex) return false;
+  slidePagination.childNodes[currentIndex].classList.remove('current');
   currentIndex -= 1;
   movePost();
+  slidePagination.childNodes[currentIndex].classList.add('current');
 }
 
 const applyResponsivePostCount = () => {
@@ -37,14 +42,24 @@ const applyResponsivePostCount = () => {
   else maxPostCount = 5;
 }
 
+const createPagination = (pageNum) => {
+  for(let i = 0; i <= pageNum - maxPostCount; i++) {
+    const li = document.createElement('li');
+    if (!i) li.classList.add('current');
+    if (slidePagination) slidePagination.appendChild(li);
+  }
+}
+
 const initializeSlide = () => {
   leftSlideBtn = document.querySelector('.slider__left');
   rightSlideBtn = document.querySelector('.slider__right');
   slidePostList = [...document.querySelectorAll('.velog.slider .velog__post')];
+  slidePagination = document.querySelector('.slider__index');
   initialPostList = slidePostList.map((slide) => slide.cloneNode(true));
   if (!leftSlideBtn || !rightSlideBtn) return;
 
   applyResponsivePostCount();
+  createPagination(slidePostList.length);
   leftSlideBtn.addEventListener('click', moveLeft);
   rightSlideBtn.addEventListener('click', moveRight);
 }

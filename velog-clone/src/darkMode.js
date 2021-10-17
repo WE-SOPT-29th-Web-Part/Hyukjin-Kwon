dcb6384cd8f85ba3cddfe262ed0cd69633c15a7a
KimKwon <khj9709@hanyang.ac.kr>
2021-10-17 16:01:38 +0900

    feat: :sparkles: 다크모드 스타일링 추가

--- velog-clone/src/darkMode.js
+++ velog-clone/src/darkMode.js
@@ -3,8 +3,11 @@ document.querySelector('.header__modeToggle').addEventListener('click', (e) => {
   if (target.classList.contains('moon')) {
     target.classList.remove('moon');
     target.classList.add('sun');
+    document.documentElement.removeAttribute('data-theme', 'dark');
+
   } else {
     target.classList.add('moon');
     target.classList.remove('sun');
+    document.documentElement.setAttribute('data-theme', 'dark');
   }
 })
